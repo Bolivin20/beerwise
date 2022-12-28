9e3805b75567df7bc7dc904ee4b43c30e9a54e8a
Nikodem Zbaraza <nikozet@interia.pl>
2022-12-28 15:40:43 +0100

    lab 9 baza danych

--- src/controllers/SecurityController.php
+++ src/controllers/SecurityController.php
@@ -2,17 +2,26 @@
 
 require_once 'AppController.php';
 require_once __DIR__ .'/../models/User.php';
+require_once __DIR__.'/../repositories/UserRepo.php';
 
 class SecurityController extends AppController
 {
     public function login(){
-        $user = new User('kuba12@gmail.com', 'admin', 'Kuba', 'Kowalski');
+
+        $userRepo = new UserRepo();
+
         if (!$this->isPost()) {
             return $this->render('login');
         }
 
         $email = $_POST['email'];
         $password = $_POST['password'];
+
+        $user = $userRepo->getUser($email);
+        
+        if (!$user) {
+            return $this->render('login', ['messages' => ['User with this email not exist!']]);
+        }
 
         if ($user->getEmail() !== $email) {
             return $this->render('login', ['messages' => ['User with this email not exist!']]);
